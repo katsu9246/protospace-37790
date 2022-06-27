1f5af59a5a2910207c8862b631c18563ab585155
katsu9246 <katsu9246@gmail.com>
2022-06-27 20:59:07 +0900

    ログイン有無でのページ参照条件の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:show]
   def show
     @user = User.find(params[:id])
+  end
+  private
+  def move_to_index
+    unless user_signed_in? && current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
   end
 end
